7cd470ec088f687a12d945e5fbad5a3a8ccb2a40
Ganes yk <ganesyudhakusuma@gmail.com>
2024-01-14 10:19:35 +0700

    Release

---
+++ assets/readme.txt
@@ -0,0 +1,2 @@
+FLUTTER MOVIE APP WITH TMDB API TUTORIAL | PAT 6 
+https://www.youtube.com/watch?v=4C8OszDrJwI
---
+++ lib/constans.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const apiKey = '791cffe59d593a0b6117d2d0d40e8970';
+  static const String imagePath = 'https://image.tmdb.org/t/p/w500';
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String imageUrlOriginal = 'https://image.tmdb.org/t/p/original';
+}
